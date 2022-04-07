0ec65a68b839be66606a41924ca1b091af679b7d
luanglaba <98131816+luanglaba@users.noreply.github.com>
2022-04-07 10:47:39 -0300

    git01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'moments/new', component: NewMomentComponent},
+  {path: 'moment-form', component: MomentFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
